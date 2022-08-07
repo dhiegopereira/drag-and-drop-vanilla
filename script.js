1b79da01da71e2d06e8c6f3ae5dbeb098c7fa862
Diego Pereira <dhiego_ti@hotmail.com>
2022-08-06 22:21:33 -0300

    finished

--- script.js
+++ script.js
@@ -1,73 +1,54 @@
-let listAll = document.querySelectorAll('.list');
+let items = document.querySelectorAll('.items');
+let titles = document.querySelectorAll('.title');
 
+let id = null;
 
+const listTitle = ['To Do', 'In Progress', 'Done'];
 
-let listId = null;
-
-const list = ['To', 'In Progress', 'Done'];
-
-const items = [
+const listItems = [
     {
+        id: 1,
         text: 'Draggable 1',
         list: 0
     },
     {
+        id: 2,
         text: 'Draggable 2',
         list: 0
     },
     {
+        id: 3,
         text: 'Draggable 3',
         list: 0
     }
 ]
 
-listAll.forEach(item => {
+titles.forEach(item => {
     item.addEventListener('dragover', (e) => e.preventDefault());
     item.addEventListener('drop', onDrop);
 });
 
-
-
-function buildlist() {
-    // listAll.forEach(item => {
-    //     item.innerHTML = '';
-    // }
-    // );
-    console.log(items);
-    items.forEach(item => {
+function buildlist() { 
+    listTitle.forEach((item, index) => {
+        titles[index].innerText = item;
+    })
+    listItems.forEach(item => {
         const div = document.createElement('div');
         div.className = 'example-draggable';
         div.innerText = item.text;
         div.draggable = "true";
-        div.addEventListener('dragstart', () => listId = item.list);
-        listAll[item.list].appendChild(div);
+        div.addEventListener('dragstart', () => id = item.id);
+        items[item.list].appendChild(div);
     });
 }
 
 buildlist();
 
-
-
 function onDrop(event) {
-    console.log(event.target.innerText);
-    const listIndex = list.findIndex(item => item.trim() === event.target.innerText.trim());
-    const itemIndex = items.findIndex(item => item.list === listId);
-    items[itemIndex].list = listIndex;
-    // console.log(items);
+    const listIndex = listTitle.findIndex(item => event.target.innerText.includes(item));
+    console.log(listIndex);
+    const itemIndex = listItems.findIndex(item => item.id === id);
+    listItems[itemIndex].list = listIndex;
+    items.forEach(item => item.innerHTML = '') 
     buildlist();
-
-
-
-    // const draggableElement = document.getElementById(id);
-
-    // console.log(id);
-
-    // const dropzone = event.target;
-
-    // dropzone.appendChild(draggableElement);
-
-    // event
-    //     .dataTransfer
-    //     .clearData();
-
 }
